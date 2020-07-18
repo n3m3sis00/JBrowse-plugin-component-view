10cd5e2bb0ee2ac2a55ac4a6c8702f9259a02225
Colin <colin.diesh@gmail.com>
2020-07-18 02:45:20 -0400

    Bar chart plugin

---
+++ src/components/index.tsx
@@ -0,0 +1,22 @@
+
+export default function (pluginManager: any) {
+  const React = pluginManager.lib["react"];
+
+  return function () {
+    const [pushed, setPushed] = React.useState();
+    return (
+      <div style={{ padding: 50 }}>
+        <h1>Hello bioinformaticians!</h1>
+        <button
+          onClick={() => {
+            setPushed("heck ya, ya pushed the button");
+          }}
+        >
+          Push the button
+        </button>
+        <p>{pushed}</p>
+      </div>
+    );
+  };
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,44 @@
 import Plugin from '@gmod/jbrowse-core/Plugin'
 import PluginManager from '@gmod/jbrowse-core/PluginManager'
+import ViewType from '@gmod/jbrowse-core/pluggableElementTypes/ViewType'
+import {
+  AbstractViewContainer,
+  isAbstractMenuManager,
+} from '@gmod/jbrowse-core/util'
+import componentFactory from './components'
 
-import ArcRenderer, {
-  configSchema as ArcRendererConfigSchema,
-  ReactComponent as ArcRendererReactComponent,
-} from './ArcRenderer'
+
+
+
 
 export default class ArcRendererPlugin extends Plugin {
-  name = 'ArcPlugin'
+  name = 'BarChartPlugin'
   install(pluginManager: PluginManager) {
-    pluginManager.addRendererType(
-      () =>
-        // @ts-ignore error "expected 0 arguments, but got 1"?
-        new ArcRenderer({
-          name: 'ArcRenderer',
-          ReactComponent: ArcRendererReactComponent,
-          configSchema: ArcRendererConfigSchema,
-        }),
+    const {types} = pluginManager.lib['mobx-state-tree']
+    const stateModel = types.model({ type: types.literal('BarChartView') }).actions(() => ({
+      setWidth(width:number) {
+      }
+    }))
+    const ReactComponent = componentFactory(pluginManager)
+
+    // const ReactComponent = () => {
+    //   return <h1>"Hello bioinformatics world!"</h1>
+    // }
+    console.log('here')
+
+    pluginManager.addViewType(
+       () => new ViewType({ name: 'BarChartView', stateModel, ReactComponent })
     )
   }
+
+  // configure(pluginManager: PluginManager) {
+  //   if (isAbstractMenuManager(pluginManager.rootModel)) {
+  //     pluginManager.rootModel.appendToSubMenu(['File', 'Add'], {
+  //       label: 'Bar chart view',
+  //       onClick: (session: AbstractViewContainer) => {
+  //         session.addView('BarChartView', {})
+  //       },
+  //     })
+  //   }
+  // }
 }
