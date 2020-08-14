44c9823fe3ee0ebda9d5920da3b3874e5966fd90
Dr. Shreya Srivastava <shreya2feb@gmail.com>
2020-08-14 21:57:30 +0530

    project setup

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,23 +1,73 @@
-// basic custom "view" component that renders a h1 and a button with react hook
-// to update state
-export default function (pluginManager: any) {
-  const React = pluginManager.lib["react"];
+import React, { useState } from "react";
+import { Tree } from "phylo-react";
 
-  return function () {
-    const [pushed, setPushed] = React.useState();
-    return (
-      <div style={{ padding: 50 }}>
-        <h1>Hello bioinformaticians!</h1>
-        <button
-          onClick={() => {
-            setPushed("heck ya, ya pushed the button");
-          }}
-        >
-          Push the button
-        </button>
-        <p>{pushed}</p>
-      </div>
-    );
-  };
+function App() {
+  const [tree, setTree] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+  const [showBranchLength, setShowBranchLength] = useState(true);
+  const [geneId, setGeneId] = useState("ENSGT00390000003602");
+  const [species, setSpecies] = useState(true);
+  return (
+    <div className="App">
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          setLoading(true);
+          const result = await (species
+            ? fetch(
+                `https://rest.ensembl.org/cafe/genetree/id/${geneId}?content-type=text/x-nh;nh_format=simple`
+              )
+            : fetch(
+                `https://rest.ensembl.org/genetree/id/${geneId}?content-type=text/x-nh;nh_format=simple`
+              ));
+          if (!result.ok) {
+            setError(result.statusText);
+            setLoading(false);
+            return;
+          }
+          const text = await result.text();
+          setTree(text);
+          setError(undefined);
+          setLoading(false);
+        }}
+      >
+        <label>
+          Enter Ensembl gene ID
+          <input
+            id="geneid"
+            value={geneId}
+            onChange={(event) => setGeneId(event.target.value)}
+            type="text"
+          />
+        </label>
+        <label>
+          Show branch length
+          <input
+            type="checkbox"
+            checked={showBranchLength}
+            onChange={(event) => {
+              setShowBranchLength(event.target.checked);
+            }}
+          />
+        </label>
+        <label>
+          Species (CAFE) tree or gene tree?
+          <input
+            type="checkbox"
+            checked={species}
+            onChange={(event) => {
+              setSpecies(event.target.checked);
+            }}
+          />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {loading ? <p>Loading...</p> : null}
+      {error ? <p style={{ color: "red" }}>{error}</p> : null}
+      {tree ? <Tree data={tree} showBranchLength={showBranchLength} /> : null}
+    </div>
+  );
 }
 
+export default App;
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,26 @@
-import Plugin from '@gmod/jbrowse-core/Plugin'
-import PluginManager from '@gmod/jbrowse-core/PluginManager'
-import ViewType from '@gmod/jbrowse-core/pluggableElementTypes/ViewType'
+import Plugin from "@gmod/jbrowse-core/Plugin";
+import PluginManager from "@gmod/jbrowse-core/PluginManager";
+import ViewType from "@gmod/jbrowse-core/pluggableElementTypes/ViewType";
+import ReactComponent from "./components";
 import {
   AbstractViewContainer,
   isAbstractMenuManager,
-} from '@gmod/jbrowse-core/util'
-import componentFactory from './components'
-
-
-
-
+} from "@gmod/jbrowse-core/util";
 
 export default class ArcRendererPlugin extends Plugin {
-  name = 'BarChartPlugin'
+  name = "BarChartPlugin";
   install(pluginManager: PluginManager) {
-    const {types} = pluginManager.lib['mobx-state-tree']
-    const stateModel = types.model({ type: types.literal('BarChartView') }).actions(() => ({
-      setWidth() {
-        // unused but required by your view
-      }
-    }))
-    const ReactComponent = componentFactory(pluginManager)
+    const { types } = pluginManager.lib["mobx-state-tree"];
+    const stateModel = types
+      .model({ type: types.literal("BarChartView") })
+      .actions(() => ({
+        setWidth() {
+          // unused but required by your view
+        },
+      }));
 
     pluginManager.addViewType(
-       () => new ViewType({ name: 'BarChartView', stateModel, ReactComponent })
-    )
+      () => new ViewType({ name: "BarChartView", stateModel, ReactComponent })
+    );
   }
 }
