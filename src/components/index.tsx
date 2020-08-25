067fa63a88710622bd929cd505c639d3ff959ae7
Shreya Srivastava <ssrivastava@bt.iitr.ac.in>
2020-08-25 11:21:01 +0000

    MSA height Issue

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -14,24 +14,24 @@ import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles(theme => ({
-  tree_div: {
-    display: 'flex',
-    flexDirection: 'row',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
-    width: 'calc(100vw - 32px)',
-  },
+    tree_div: {
+        display: 'flex',
+        flexDirection: 'row',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+        width: 'calc(100vw - 32px)',
+    },
 }))
 
 var globalseq = ""
 function ensemblToStock(currentNode) {
     var i, currentChild;
     for (i = 0; i < 2; i += 1) {
-        if(currentNode.children){
+        if (currentNode.children) {
             currentChild = currentNode.children[i];
             ensemblToStock(currentChild);
-        }else{
+        } else {
             globalseq += currentNode.sequence.id[0].accession + " " + currentNode.sequence.mol_seq.seq + "\n";
             return;
         }
@@ -39,90 +39,91 @@ function ensemblToStock(currentNode) {
     }
 }
 
-function TreeMSA(props){
-  const {treedata, msadata, layout} = props
-  const [treeresponse, setTreeresponse] = useState(null)
-  const classes = useStyles()
+function TreeMSA(props) {
+    const { treedata, msadata, layout, showBranchLength } = props
+    const [treeresponse, setTreeresponse] = useState(null)
+    const classes = useStyles()
 
-  return (
-      <Grid key={1} item>
-        <div className={classes.tree_div}>
-          <Box width="50%">
-            <Tree 
-              data = {treedata}
-              layout = {layout}
-              getConfig={treeresponse === null ? setTreeresponse : d => {}}/>
-          </Box>
-          <Box width="50%" style={{ overflowX: 'scroll' }}>
-          {treeresponse !== null ? <MSAsvg data={msadata} heigtoftree={treeresponse.treeheight} dataToShow={treeresponse.leafloc}/> : null}
-          </Box>
-        </div>
-      </Grid>
-  )
+    return (
+        <Grid key={1} item>
+            <div className={classes.tree_div}>
+                <Box width="30%">
+                    <Tree
+                        data={treedata}
+                        layout={layout}
+                        showBranchLength={showBranchLength}
+                        getConfig={treeresponse === null ? setTreeresponse : d => { }} />
+                </Box>
+                <Box width="70%" style={{ overflowX: 'scroll' }}>
+                    {treeresponse !== null ? <MSAsvg data={msadata} heightoftree={treeresponse.treeheight} dataToShow={treeresponse.leafloc} /> : null}
+                </Box>
+            </div>
+        </Grid>
+    )
 }
 
 function App() {
-  const [tree, setTree] = useState();
-  const [msadata, setmsadata] = useState();
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState();
-  const [showBranchLength, setShowBranchLength] = useState(true);
-  const [geneId, setGeneId] = useState("ENSGT00390000003602");
-  const [species, setSpecies] = useState(false);
-  return (
-    <div className="App">
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
-          setLoading(true);
-          const result = await fetch(
-                `https://rest.ensembl.org/genetree/id/${geneId}?content-type=text/x-nh;nh_format=simple`
-              );
+    const [tree, setTree] = useState();
+    const [msadata, setmsadata] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
+    const [showBranchLength, setShowBranchLength] = useState(true);
+    const [geneId, setGeneId] = useState("ENSGT00390000003602");
+    const [species, setSpecies] = useState(false);
+    return (
+        <div className="App">
+            <form
+                onSubmit={async (event) => {
+                    event.preventDefault();
+                    setLoading(true);
+                    const result = await fetch(
+                        `https://rest.ensembl.org/genetree/id/${geneId}?content-type=text/x-nh;nh_format=simple`
+                    );
 
-          const resultmsa = await fetch(
-                `https://rest.ensembl.org/genetree/id/${geneId}?content-type=application/json;aligned=1;sequence=cdna` //
-              );
-              
-          if (!result.ok && !resultmsa.ok) {
-            setError(result.statusText);
-            setLoading(false);
-            return;
-          }
-          const text = await result.text();
-          const msatext = await resultmsa.json();
-          await ensemblToStock(msatext.tree)
-          setTree(text);
-          setmsadata(globalseq)
-          setError(undefined);
-          setLoading(false);
-        }}
-      >
-        <label>
-          Enter Ensembl gene ID
+                    const resultmsa = await fetch(
+                        `https://rest.ensembl.org/genetree/id/${geneId}?content-type=application/json;aligned=1;sequence=cdna` //
+                    );
+
+                    if (!result.ok && !resultmsa.ok) {
+                        setError(result.statusText);
+                        setLoading(false);
+                        return;
+                    }
+                    const text = await result.text();
+                    const msatext = await resultmsa.json();
+                    await ensemblToStock(msatext.tree)
+                    setTree(text);
+                    setmsadata(globalseq)
+                    setError(undefined);
+                    setLoading(false);
+                }}
+            >
+                <label>
+                    Enter Ensembl gene ID
+                <input
+                        id="geneid"
+                        value={geneId}
+                        onChange={(event) => setGeneId(event.target.value)}
+                        type="text"
+                    />
+                </label>
+                <label>
+                    Show branch length
           <input
-            id="geneid"
-            value={geneId}
-            onChange={(event) => setGeneId(event.target.value)}
-            type="text"
-          />
-        </label>
-        <label>
-          Show branch length
-          <input
-            type="checkbox"
-            checked={showBranchLength}
-            onChange={(event) => {
-              setShowBranchLength(event.target.checked);
-            }}
-          />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-      {loading ? <p>Loading...</p> : null}
-      {error ? <p style={{ color: "red" }}>{error}</p> : null}
-      {tree ? <TreeMSA treedata={tree} msadata={msadata} showBranchLength={showBranchLength} layout={"linear"}/> : null}
-    </div>
-  );
+                        type="checkbox"
+                        checked={showBranchLength}
+                        onChange={(event) => {
+                            setShowBranchLength(event.target.checked);
+                        }}
+                    />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+            {loading ? <p>Loading...</p> : null}
+            {error ? <p style={{ color: "red" }}>{error}</p> : null}
+            {tree ? <TreeMSA treedata={tree} msadata={msadata} showBranchLength={showBranchLength} layout={"linear"} /> : null}
+        </div>
+    );
 }
 
 export default App;
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,24 @@ import PluginManager from "@gmod/jbrowse-core/PluginManager";
 import ViewType from "@gmod/jbrowse-core/pluggableElementTypes/ViewType";
 import ReactComponent from "./components";
 import {
-  AbstractViewContainer,
-  isAbstractMenuManager,
+    AbstractViewContainer,
+    isAbstractMenuManager,
 } from "@gmod/jbrowse-core/util";
 
 export default class ArcRendererPlugin extends Plugin {
-  name = "PhyloTreePlugin";
-  install(pluginManager: PluginManager) {
-    const { types } = pluginManager.lib["mobx-state-tree"];
-    const stateModel = types
-      .model({ type: types.literal("PhyloTreeView") })
-      .actions(() => ({
-        setWidth() {
-          // unused but required by your view
-        },
-      }));
+    name = "PhyloTreePlugin";
+    install(pluginManager: PluginManager) {
+        const { types } = pluginManager.lib["mobx-state-tree"];
+        const stateModel = types
+            .model({ type: types.literal("PhyloTreeView") })
+            .actions(() => ({
+                setWidth() {
+                    // unused but required by your view
+                },
+            }));
 
-    pluginManager.addViewType(
-      () => new ViewType({ name: "PhyloTreeView", stateModel, ReactComponent })
-    );
-  }
+        pluginManager.addViewType(
+            () => new ViewType({ name: "PhyloTreeView", stateModel, ReactComponent })
+        );
+    }
 }
